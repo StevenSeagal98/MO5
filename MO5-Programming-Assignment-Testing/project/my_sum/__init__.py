797fa76561991d329007b499958e8e44acac5c4e
nateorona98 <nathaniel.orona98@gmail.com>
2022-12-03 16:07:36 -0600

    moved files into folder

---
+++ MO5-Programming-Assignment-Testing/project/my_sum/__init__.py
@@ -0,0 +1,9 @@
+# https://realpython.com/python-testing/#testing-your-code
+# MO5 Programming Assignment - Writing Your First Test
+# Nate Orona
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
